1711509db5886f1c0f60515fbb7966d17515bb0c
smart-parking-2026 <maouimarwa7@gmail.com>
2026-02-20 15:26:21 +0100

    Initialize Flask app with a home route

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Smart Parking is Online!</h1><p>The connection between GitHub and Render is successful.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
